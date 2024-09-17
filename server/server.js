951b9dc30442b0eb9e30ba74cb0821db343d8c75
JM344 <JAVIMARI0411@GMAIL.COM>
2024-09-16 21:23:22 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
+// Import the Express module
 const express = require('express');
 
+// Initialize the Express application
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;  // Define the port to listen on
 
+// Serve static files from the 'client/dist' directory
 app.use(express.static('../client/dist'));
+
+// Middleware to parse URL-encoded data and JSON data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Import and apply HTML routes
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+// Start the server and listen on the defined port
+app.listen(PORT, () => {
+  console.log(`Now listening on port: ${PORT}`);
+});
